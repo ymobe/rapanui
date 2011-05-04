1382810c14518ac27538cc9294de4d4818451876
Stefano Linguerri <stefano.linguerri@gmail.com>
2011-05-05 00:15:46 +0200

    first slide effect on RNDirector

--- RNDirector.lua
+++ RNDirector.lua
@@ -6,19 +6,25 @@
 ----------------------------------------------------------------
 module(..., package.seeall)
 
---require("RNUtil")
-
-
 SWAP = 1
 SLIDE_TO_LEFT = 2
+SLIDE_TO_RIGHT = 3
+SLIDE_TO_BOTTOM = 4
+SLIDE_TO_TOP = 5
+
+local TRANSITIONING = false
+
+offsetX = 0
+offsetY = 0
+
+currentScene = nil
+nextScene = nil
 
 -- Create a new RNDirector Object
 function RNDirector:new(o)
 
     o = o or {
         name = "",
-        currentScene,
-        nextScene,
         scenes = {}
     }
 
@@ -28,7 +34,7 @@ function RNDirector:new(o)
 end
 
 --
--- add a scene to the Director the name of the scene have to be unique because it will be used to identify the object
+-- add a scene to the Director the name of the scene have to be unique because it will be used ad key
 --
 -- @param scene  a scene object to add to the director managed scenes
 
@@ -49,7 +55,7 @@ function RNDirector:startWithScene(name)
     do
         if (key == name) then
             print("In iteration " .. value:getName())
-            self.currentScene = value
+            currentScene = value
         else
             value:hideAll()
         end
@@ -61,48 +67,142 @@ function RNDirector:getScene(name)
 end
 
 
+--
+-- Main method for switching
+--
+-- @param mode the switch mode if the mode it's not valid then a simple swap it's performed
+--
 function RNDirector:switch(mode, name)
+    if (TRANSITIONING) then
+        return
+    end
+
     if (mode == SWAP) then
         self:swap(name)
     elseif (mode == SLIDE_TO_LEFT) then
         self:slideToLeft(name)
+    elseif (mode == SLIDE_TO_RIGHT) then
+        self:slideToRight(name)
+    elseif (mode == SLIDE_TO_BOTTOM) then
+        self:slideToBottom(name)
+    elseif (mode == SLIDE_TO_TOP) then
+        self:slideToTop(name)
     else
         self:swap(name)
     end
 end
 
+--
+-- a simple scene swap, no effect
+--
+-- @param name the name of the scene
+--
+
 function RNDirector:swap(name)
-    self.currentScene:hideAll()
+    currentScene:hideAll()
     local newScene = self:getScene(name)
-    self.currentScene = newScene
-    self.currentScene:showAll()
+    currentScene = newScene
+    currentScene:showAll()
+end
+
+function RNDirector:slideToTop(name)
+    offsetX = 0
+    offsetY = -480
+    self:slide(name, offsetX, offsetY)
+end
+
+function RNDirector:slideToBottom(name)
+    offsetX = 0
+    offsetY = 480
+    self:slide(name, offsetX, offsetY)
 end
 
 function RNDirector:slideToLeft(name)
---self.currentScene:hideAll()
-    print("Slide to left")
-    local currentSceneSprites = self.currentScene:getSprites()
-    nextScene = self:getScene(name)
-
-    nextScene:showAll()
-
-    local action = nil
-
-    --print_r(currentSceneSprites)
-    for key, value in pairs(currentSceneSprites)
-    do
-        print("Name: " .. value:getImageName())
-        action = value:getProp():moveLoc(-320, 0, 1)
-    end
-
-    if (action ~= nil) then
-        action:setListener(MOAIAction.EVENT_STOP, self.onEndSlideToLeft)
-    end
---	self.currentScene:showAll()
+    offsetX = -320
+    offsetY = 0
+    self:slide(name, offsetX, offsetY)
 end
 
-function RNDirector:onEndSlideToLeft()
-    print("Swapped onEndSlideToLeft")
+function RNDirector:slideToRight(name)
+    offsetX = 320
+    offsetY = 0
+    self:slide(name, offsetX, offsetY)
+end
+
+function RNDirector:slide(name, offsetX, offsetY)
+
+
+
+    if (not TRANSITIONING) then
+        TRANSITIONING = true
+
+        nextScene = self:getScene(name)
+
+        local nextSceneSprites = nextScene:getSprites()
+
+        for key, value in pairs(nextSceneSprites)
+        do
+            local xLoc, yLoc = value:getProp():getLoc()
+            xLoc = xLoc + (-1) * offsetX
+            yLoc = yLoc + (-1) * offsetY
+
+            print("slide:  xLoc: " .. xLoc)
+
+            value:getProp():setLoc(xLoc, yLoc)
+        end
+
+        nextScene:showAll()
+
+        local action = nil
+
+        --print_r(currentSceneSprites)
+        for key, value in pairs(nextSceneSprites)
+        do
+        --  print("Name: " .. value:getImageName())
+            value:getProp():moveLoc(offsetX, offsetY, 1)
+        end
+
+        --    MOAISim.pushRenderPass(self.layer)
+
+        local action = nil
+
+        --print_r(currentSceneSprites)
+
+        local currentSceneSprites = currentScene:getSprites()
+        for key, value in pairs(currentSceneSprites)
+        do
+            print("Name: " .. value:getImageName())
+            action = value:getProp():moveLoc(offsetX, offsetY, 1)
+        end
+
+        -- add a listener on the last prop moveLoc to do task at the end of animation
+        if (action ~= nil) then
+            action:setListener(MOAIAction.EVENT_STOP, self.onEndSlide)
+        end
+        currentScene:showAll()
+    end
+end
+
+--
+-- Reset scenes status at the end of the transition
+--
+--
+function RNDirector:onEndSlide()
+    currentScene:hideAll()
+
+    local currentSceneSprites = currentScene:getSprites()
+    for key, value in pairs(currentSceneSprites)
+    do
+        local xLoc, yLoc = value:getProp():getLoc()
+        xLoc = xLoc + (-1) * offsetX
+        yLoc = yLoc + (-1) * offsetY
+
+        print(" onEndSlide " .. currentScene:getName() .. " xLoc: " .. xLoc .. " yLoc: " .. yLoc)
+
+        value:getProp():setLoc(xLoc, yLoc)
+    end
+
     currentScene = nextScene
     nextScene = nil
+    TRANSITIONING = false
 end
--- RNScene.lua
+++ RNScene.lua
@@ -12,81 +12,84 @@ module(..., package.seeall)
 
 function RNScene:new(o)
 
-	o = o or {
-		name = "",
-		sprites = {},
-		width = 0,
-		height = 0,
-		spriteIndex = 0,
-		viewport = nil,
-		layer = nil,
-		visible = true
-	}
+    o = o or {
+        name = "",
+        sprites = {},
+        width = 0,
+        height = 0,
+        spriteIndex = 0,
+        viewport = nil,
+        layer = nil,
+        visible = true
+    }
 
-	setmetatable(o, self)
-	self.__index = self
-	return o
+    setmetatable(o, self)
+    self.__index = self
+    return o
 end
 
 function RNScene:getName()
-	return self.name
+    return self.name
 end
 
 function RNScene:setName(name)
-	self.name = name
+    self.name = name
 end
 
 function RNScene:initWith(width, height)
-	self.width = width
-	self.height = height
-	self.viewport = MOAIViewport.new()
-	self.viewport:setSize(width, height)
-	self.viewport:setScale(width, -height)
-	self.viewport:setOffset(-1, 1)
-	self.layer = MOAILayer2D.new()
-	self.layer:setViewport(self.viewport)
-	MOAISim.pushRenderPass(self.layer)
+    self.width = width
+    self.height = height
+    self.viewport = MOAIViewport.new()
+    self.viewport:setSize(width, height)
+    self.viewport:setScale(width, -height)
+    self.viewport:setOffset(-1, 1)
+    self.layer = MOAILayer2D.new()
+    self.layer:setViewport(self.viewport)
+    MOAISim.pushRenderPass(self.layer)
 end
 
 
 function RNScene:addSpriteWithLocatingMode(sprite, mode)
-	sprite:setScreenSize(self.width, self.height)
-	sprite:setLocatingMode(mode)
-	self.layer:insertProp(sprite:getProp())
-	sprite:updateLocation()
-	self.sprites[self.spriteIndex] = sprite
-	self.spriteIndex = self.spriteIndex + 1
+    sprite:setScreenSize(self.width, self.height)
+    sprite:setLocatingMode(mode)
+    self.layer:insertProp(sprite:getProp())
+    sprite:updateLocation()
+    self.sprites[self.spriteIndex] = sprite
+    self.spriteIndex = self.spriteIndex + 1
 end
 
+function RNScene:getLayer()
+    return self.layer
+end
 
 function RNScene:addSprite(sprite)
-	self:addSpriteWithLocatingMode(sprite, RNSprite.CENTERED_MODE)
+    self:addSpriteWithLocatingMode(sprite, RNSprite.CENTERED_MODE)
 end
 
 function RNScene:getSprites()
-	return self.sprites
+    return self.sprites
 end
 
 
 function RNScene:hideAll()
-	self.visible = false
-	for key, value in pairs(self.sprites)
-	do
-		print("Have to remove " .. value:getImageName())
-		self.layer:removeProp(value:getProp())
-	end
+    self.visible = false
+    for key, value in pairs(self.sprites)
+    do
+      --  print("Have to remove " .. value:getImageName())
+        self.layer:removeProp(value:getProp())
+    end
 end
 
 function RNScene:isVisible()
-	return self.visible
+    return self.visible
 end
 
 function RNScene:showAll()
-	self.visible = true
-	for key, value in pairs(self.sprites)
-	do
-		print("Have to remove " .. value:getImageName())
-		self.layer:insertProp(value:getProp())
-	end
-	print("Have to show all")
+    self.visible = true
+    for key, value in pairs(self.sprites)
+    do
+      --  print("Have to remove " .. value:getImageName())
+        self.layer:insertProp(value:getProp())
+    end
+ --   print("Have to show all")
 end
