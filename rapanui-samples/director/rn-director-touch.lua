543720a8d25d7d053db8ee27b4759e175e7cbbee
eljeko <stefano.linguerri@gmail.com>
2011-12-26 02:38:41 +0100

    fix on director demo touch

---
+++ rapanui-samples/director/rn-director-touch.lua
@@ -0,0 +1,56 @@
+------------------------------------------------------------------------------------------------------------------------
+--
+-- RapaNui
+--
+-- by Ymobe ltd  (http://ymobe.co.uk)
+--
+-- LICENSE:
+--
+-- RapaNui uses the Common Public Attribution License Version 1.0 (CPAL) http://www.opensource.org/licenses/cpal_1.0.
+-- CPAL is an Open Source Initiative approved
+-- license based on the Mozilla Public License, with the added requirement that you attribute
+-- Moai (http://getmoai.com/) and RapaNui in the credits of your program.
+--
+------------------------------------------------------------------------------------------------------------------------
+
+effects = { "slidetoleft", "slidetoright", "slidetotop", "slidetobottom", "pop", "fade" }
+
+director = RNDirector:new()
+
+director:addScene("rapanui-samples/director/scene1")
+director:addScene("rapanui-samples/director/scene2")
+
+--check if they are inside the director
+for i, v in ipairs(director.scenes) do
+    print(i, v)
+end
+
+--Set director's time
+director:setTime(1000)
+
+director:showScene("rapanui-samples/director/scene1", "pop")
+
+
+local scene1 = "rapanui-samples/director/scene1"
+local scene2 = "rapanui-samples/director/scene2"
+local sceneToShow = scene1
+
+function onTouchEvent(event)
+    if event.phase == "began" and director:isTransitioning() == false then
+        if sceneToShow == scene1 then
+            sceneToShow = scene2
+        else
+            sceneToShow = scene1
+        end
+
+        director:showScene(sceneToShow, getRandomTransition())
+    end
+end
+
+function getRandomTransition()
+    return effects[math.random(1, 6)]
+end
+
+RNListeners:addEventListener("touch", onTouchEvent)
+
+
