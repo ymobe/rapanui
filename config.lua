9ae1441828f21bcc332dde124eb5daebbd7d8917
eljeko <stefano.linguerri@gmail.com>
2012-02-15 21:56:24 +0100

    merge

--- config.lua
+++ config.lua
@@ -15,22 +15,22 @@
 module(..., package.seeall)
 
 local landscape = false --true
-local device = "iPad"
+local device = "iPhone"
 local sizes = {}
-sizes["iPad"] = {768,1024,384,512}
-sizes["iPadreal"] = {768,1024,768,1024}
-sizes["iPhone"] = {320,480,503,670}
+sizes["iPad"] = { 768, 1024, 384, 512 }
+sizes["iPadreal"] = { 768, 1024, 768, 1024 }
+sizes["iPhone"] = { 320, 480, 320, 480, }
 
 
 local sw, sh = MOAIEnvironment.getScreenSize()
 if sw ~= 0 then
-	PW, PH, SW, SH = sw, sh, sw, sh
+    PW, PH, SW, SH = sw, sh, sw, sh
 else
-	PW, PH, SW, SH = sizes[device][1],sizes[device][2], sizes[device][3], sizes[device][4]
+    PW, PH, SW, SH = sizes[device][1], sizes[device][2], sizes[device][3], sizes[device][4]
 end
 sw, sh = nil, nil
 
 if landscape == true then -- flip Widths and Hieghts
-	PW, PH = PH, PW
-	SW, SH = SH, SW
+    PW, PH = PH, PW
+    SW, SH = SH, SW
 end
