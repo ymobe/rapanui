d664780b48f505797a205d4b72a3797ec9145c61
eljeko <stefano.linguerri@gmail.com>
2012-02-22 23:34:17 +0100

    new dir rapanui-sdk and refactoring to remove module(..., package.seeall)

---
+++ rapanui-sdk/rapanui.lua
@@ -0,0 +1,34 @@
+--[[
+--
+-- RapaNui
+--
+-- by Ymobe ltd  (http://ymobe.co.uk)
+--
+-- LICENSE:
+--
+-- RapaNui uses the Common Public Attribution License Version 1.0 (CPAL) http://www.opensource.org/licenses/cpal_1.0.
+-- CPAL is an Open Source Initiative approved
+-- license based on the Mozilla Public License, with the added requirement that you attribute
+-- Moai (http://getmoai.com/) and RapaNui in the credits of your program.
+]]
+
+RNUtil = require("rapanui-sdk/RNUtil")
+RNUnit = require("rapanui-sdk/RNUnit")
+RNThread = require("rapanui-sdk/RNThread")
+RNGroup = require("rapanui-sdk/RNGroup")
+RNFactory = require("rapanui-sdk/RNFactory")
+RNListeners = require("rapanui-sdk/RNListeners")
+RNTransition = require("rapanui-sdk/RNTransition")
+RNMainThread = require("rapanui-sdk/RNMainThread")
+RNDirector = require("rapanui-sdk/RNDirector")
+RNPhysics = require("rapanui-sdk/RNPhysics")
+RNInputManager = require("rapanui-sdk/RNInputManager")
+
+RNMapFactory = require("rapanui-sdk/RNMapFactory")
+RNMap = require("rapanui-sdk/RNMap")
+RNMapLayer = require("rapanui-sdk/RNMapLayer")
+RNMapObject = require("rapanui-sdk/RNMapObject")
+RNMapObjectGroup = require("rapanui-sdk/RNMapObjectGroup")
+RNMapTileset = require("rapanui-sdk/RNMapTileset")
+RNTiledMapParser = require("rapanui-sdk/RNTiledMapParser")
+RNTiledLuaMapParser = require("rapanui-sdk/RNTiledLuaMapParser")
