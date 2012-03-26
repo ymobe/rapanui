9a354f3ac7d84ac8e21591049bb7409262b5ceb0
MattiaFortunati <mattfortunati@gmail.com>
2012-03-26 11:48:34 +0200

    ec

--- rapanui-sdk/RNFactory.lua
+++ rapanui-sdk/RNFactory.lua
@@ -44,7 +44,7 @@ RNFactory.height = 0
 function RNFactory.init()
 
     local lwidth, lheight, screenlwidth, screenHeight
-    local screenX, screenY = 0,0
+    local screenX, screenY = 0,0   --mocked
 
     if screenX ~= 0 then --if physical screen
         lwidth, lheight, screenlwidth, screenHeight = screenX, screenY, screenX, screenY
