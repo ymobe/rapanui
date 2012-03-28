d7380a18f024a64e65b3323c0ee76e35ec37ba61
MattiaFortunati <mattfortunati@gmail.com>
2012-03-28 09:45:08 +0200

    MOAIEnvironment fix closes #33

--- rapanui-sdk/RNFactory.lua
+++ rapanui-sdk/RNFactory.lua
@@ -44,9 +44,9 @@ RNFactory.height = 0
 function RNFactory.init()
 
     local lwidth, lheight, screenlwidth, screenHeight
-    local screenX, screenY = 0,0   --mocked (implementation needs asap)
+    local screenX, screenY = MOAIEnvironment.screenWidth, MOAIEnvironment.screenHeight
 
-    if screenX ~= 0 then --if physical screen
+    if screenX ~= nil then --if physical screen
         lwidth, lheight, screenlwidth, screenHeight = screenX, screenY, screenX, screenY
     else
         lwidth, lheight, screenlwidth, screenHeight = config.sizes[config.device][1], config.sizes[config.device][2], config.sizes[config.device][3], config.sizes[config.device][4]
